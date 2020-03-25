3ae8df5b96d3dc76e1ee9c154850f13dc6f5bda4
Edem Ahorlu <edem.ahorlu@duke.edu>
2020-03-24 23:25:42 -0400

    p4 modified

--- src/LinkStrand.java
+++ src/LinkStrand.java
@@ -1,3 +1,88 @@
-public class LinkStrand {
-    //Shane Bishop
+public class LinkStrand implements IDnaStrand {
+
+    private class Node {
+        String info;
+        Node next;
+
+        public Node(String s, Node n) {
+            info = s;
+            next = n;
+        }
+    }
+
+    private Node myFirst, myLast;
+    private long mySize;
+    private int myAppends;
+
+    public LinkStrand(String s) {
+        initialize(s);
+    }
+
+    public LinkStrand() {
+        this("");
+    }
+
+    @Override
+    public void initialize(String source) {
+        myFirst = new Node(source, null);
+        myLast = myFirst;
+        mySize = source.length();
+        myAppends = 0;
+    }
+
+    @Override
+    public IDnaStrand getInstance(String source) {
+        return new LinkStrand(source);
+    }
+
+    @Override
+    public long size() {
+        return mySize;
+    }
+
+    @Override
+    public IDnaStrand append(String dna) {
+        myLast.next = new Node (dna,null);
+        myLast = myLast.next;
+        mySize +=dna.length();
+        myAppends++;
+        return this;
+    }
+
+    @Override
+    public IDnaStrand reverse() {
+        StringBuilder reversed = new StringBuilder(myLast.info);
+        LinkStrand copy = new LinkStrand(reversed.reverse().toString());
+
+
+
+
+        copy.reverse()
+
+        return null;
+    }
+
+    @Override
+    public int getAppendCount() {
+        return 0;
+    }
+
+    @Override
+    public char charAt(int index) {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        Node temp = myFirst;
+        StringBuilder hold = new StringBuilder();
+        while (temp != null) {
+            hold.append(temp.info);
+            temp = temp.next;
+        }
+        return hold.toString();
+    }
+
+
+
 }
