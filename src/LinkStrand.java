5ecc94ee8ebcd356bd90d8e90efd95df767a8315
Shane Bishop <shane.bishop@duke.edu>
2020-03-26 19:16:51 -0400

    edited5

--- src/LinkStrand.java
+++ src/LinkStrand.java
@@ -112,7 +112,7 @@ public class LinkStrand implements IDnaStrand {
      */
     @Override
     public char charAt(int index) {
-        if(index <= 0 || index>mySize-1){
+        if(index <= 0 | index>mySize-1){
             throw new IndexOutOfBoundsException("out of bounds");
         }
         if(index < myIndex){
